15f803a21fbb65427f123019f423bd7517171ff4
Swarnendu Narayan Chakraborty <rohanchakraborty197@gmail.com>
2025-12-07 20:15:43 +0530

    Update server.js
    password update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "BABU@08ram",
+    password: "Narayan@2004",
     database: "tracknheal_db"
 });
 
